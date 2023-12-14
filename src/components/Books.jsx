a21e84bb5e80082a434e3f1250a6fe28c94017e3
fk4321 <139650641+fk4321@users.noreply.github.com>
2023-12-14 11:15:27 -0500

    displayed list of book titles

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,12 +1,32 @@
-
+import { useState, useEffect } from "react"
+import axios from "axios"
 
 const Books = () => {
+    //state variables 
+    const [books, setBooks] = useState([])
+
+    //fetch books
+    useEffect(() => {
+        const fetchBooks = async () => {
+            const response = await axios.get('https://fsa-book-buddy-b6e748d1380d.herokuapp.com/api/books')
+            console.log(response.data.books)
+            //update state
+            setBooks(response.data.books)
+        }
+        fetchBooks()
+    },[])
 
     return(
         <div>
 
             <h1>Books</h1>
-            <h3>We should see our books here!</h3>
+            {books.map((book) => {
+                return (
+                    <ul key={book.id}>
+                        <li>{book.title}</li>
+                    </ul>
+                )
+            })}
         
         </div>
     )
